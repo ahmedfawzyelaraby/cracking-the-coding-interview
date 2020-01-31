0df43461f6252b5f8277df418448c6e36eb15fa8
ahmedfawzyelaraby <ahmedfawzyelaraby@gmail.com>
2020-01-31 14:49:30 +0200

    [feat]: add hash map based is string characters unique solution

--- chapter-1/p-1.1/src/main.cpp
+++ chapter-1/p-1.1/src/main.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <iostream>
+#include <map>
 
 bool isUniqueDoubleLoop(std::string input) {
     for(int i = 0; i < (input.length() - 1); i++) {
@@ -13,11 +14,28 @@ bool isUniqueDoubleLoop(std::string input) {
 }
 
 bool isUniqueHashTable(std::string input) {
-    
+    std::map<char, int> occurance_map;
+    for(int i = 0; i < input.length(); i++) {
+        if(occurance_map.find(input[i]) != occurance_map.end())
+            occurance_map[input[i]]++;
+        else
+            occurance_map[input[i]] = 1;
+    }
+
+    for(std::map<char, int>::iterator i = occurance_map.begin(); i != occurance_map.end(); i++) {
+        if(i->second > 1)
+            return false;
+    }
+
+    return true;
 }
 
 int main(int argc, char** argv) {
     std::string string_to_check(argv[1]);
 
     std::cout << isUniqueDoubleLoop(string_to_check) << std::endl;
+
+    std::cout << isUniqueHashTable(string_to_check) << std::endl;
+
+    return 0;
 }
